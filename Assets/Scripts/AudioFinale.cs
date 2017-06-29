ecb7720d482de4e2fe7cff8bbfd953b09950f116
JuanpabloP <giampaolo.piccini@gmail.com>
2017-06-29 16:20:42 +0200

    Fix Finale

---
+++ Assets/Scripts/AudioFinale.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AudioFinale : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Actual Level")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
